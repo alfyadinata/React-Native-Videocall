ff3bc2c447dceb6de4d1aec6ca5af55eff0b8f2f
alfy adinata <42163566+alfyadinata@users.noreply.github.com>
2021-01-01 20:21:11 +0700

    static screen for call history

--- src/screens/CallHistory/index.js
+++ src/screens/CallHistory/index.js
@@ -1,22 +1,89 @@
 import React from 'react'
 import { View, Text, StyleSheet, Image } from 'react-native'
 import { ScrollView } from 'react-native-gesture-handler'
-import { Menu } from 'react-native-paper'
+import { List } from 'react-native-paper'
 import { colors } from '../../styles/colors'
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons'
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 
-const CallHistory = () => {
+const Tab = createMaterialTopTabNavigator();
+
+const All = () => {
     return (
         <View style={styles.container}>
             <ScrollView showsVerticalScrollIndicator={false}>
                 <View style={styles.row}>
-                    <Menu.Item icon="redo" onPress={() => { }} title="Redo" />
-                    <Menu.Item icon="undo" onPress={() => { }} title="Undo" />
-                    <Menu.Item icon="content-cut" onPress={() => { }} title="Cut" disabled />
-                    <Menu.Item icon="content-copy" onPress={() => { }} title="Copy" disabled />
-                    <Menu.Item icon="content-paste" onPress={() => { }} title="Paste" />
+                    <List.Item
+                        title="First Item"
+                        description="Item description"
+                        left={() => <MaterialCommunityIcons name="account-circle" size={35} color={colors.primary} />}
+                        right={() => <MaterialCommunityIcons name="call-missed" size={35} color={colors.secondary} />}
+                    />
+                    <List.Item
+                        title="First Item"
+                        description="Item description"
+                        left={() => <MaterialCommunityIcons name="account-circle" size={35} color={colors.primary} />}
+                        right={() => <MaterialCommunityIcons name="call-made" size={35} color={colors.green} />}
+                    />
                 </View>
             </ScrollView>
         </View>
+    )
+}
+
+const Missed = () => {
+    return (
+        <View style={styles.container}>
+            <ScrollView showsVerticalScrollIndicator={false}>
+                <View style={styles.row}>
+                    <List.Item
+                        title="First Item"
+                        description="Item description"
+                        left={() => <MaterialCommunityIcons name="account-circle" size={35} color={colors.primary} />}
+                        right={() => <MaterialCommunityIcons name="call-missed" size={35} color={colors.secondary} />}
+                    />
+                    <List.Item
+                        title="First Item"
+                        description="Item description"
+                        left={() => <MaterialCommunityIcons name="account-circle" size={35} color={colors.primary} />}
+                        right={() => <MaterialCommunityIcons name="call-made" size={35} color={colors.green} />}
+                    />
+                </View>
+            </ScrollView>
+        </View>
+    )
+}
+
+const Connected = () => {
+    return (
+        <View style={styles.container}>
+            <ScrollView showsVerticalScrollIndicator={false}>
+                <View style={styles.row}>
+                    <List.Item
+                        title="First Item"
+                        description="Item description"
+                        left={() => <MaterialCommunityIcons name="account-circle" size={35} color={colors.primary} />}
+                        right={() => <MaterialCommunityIcons name="call-missed" size={35} color={colors.secondary} />}
+                    />
+                    <List.Item
+                        title="First Item"
+                        description="Item description"
+                        left={() => <MaterialCommunityIcons name="account-circle" size={35} color={colors.primary} />}
+                        right={() => <MaterialCommunityIcons name="call-made" size={35} color={colors.green} />}
+                    />
+                </View>
+            </ScrollView>
+        </View>
+    )
+}
+
+const CallHistory = () => {
+    return (
+        <Tab.Navigator>
+            <Tab.Screen name="All" component={All} />
+            <Tab.Screen name="Missed" component={Missed} />
+            <Tab.Screen name="Connected" component={Connected} />
+        </Tab.Navigator>
     )
 }
 
