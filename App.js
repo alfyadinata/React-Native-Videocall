d769c5c9087a1452b53d27d6449fdee61fe2d69f
alfy adinata <42163566+alfyadinata@users.noreply.github.com>
2021-01-09 17:52:26 +0700

    status bar color

--- App.js
+++ App.js
@@ -2,11 +2,12 @@ import React, { Fragment } from 'react'
 import FlashMessage from "react-native-flash-message"
 import { StatusBar } from 'react-native'
 import AppNavigator from './src/router'
+import { colors } from './src/styles/colors'
 
 function App() {
   return (
     <Fragment>
-      <StatusBar barStyle="light-content" backgroundColor="#182C61" />
+      <StatusBar barStyle="light-content" backgroundColor={colors.secondary} />
       <AppNavigator />
       <FlashMessage position="top" />
     </Fragment>
